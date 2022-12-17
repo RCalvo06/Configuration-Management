87712b953ccc21f57e52a9e0df6c31bf439d8179
rcalvo06 <ricardocalvo0601@gmail.com>
2022-12-17 10:31:30 -0600

    fix ssh key v2

--- locals.tf
+++ locals.tf
@@ -1,7 +1,8 @@
 locals {
   vpc_id           = "vpc-0217106027adaab03"
   ssh_user         = "ubuntu"
-  key_name         = "tmp/keys/id_rsa"
+  key_name         = "devops"
+  private_key_path = "/tmp/keys/id_rsa"
   ami_id           = "ami-0574da719dca65348"
   instance_type    = "t2.micro"
 }
